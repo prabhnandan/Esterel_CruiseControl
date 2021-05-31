e6aabc97da1f5126bd982da133dab8fa64239ed4
prabhnandan <psin546@aucklanduni.ac.nz>
2021-05-31 16:45:57 +1200

    First commit

---
+++ files/cruiseControl.h
@@ -0,0 +1,9 @@
+#define PedalsMin 3.00
+#define SpeedMin 30.00
+#define SpeedMax 150.00
+
+#define Zero 0.00
+
+#define SpeedInc 2.50
+
+float regulateThrottle(int isGoingOn, float cruiseSpeed, float vehicleSpeed);
